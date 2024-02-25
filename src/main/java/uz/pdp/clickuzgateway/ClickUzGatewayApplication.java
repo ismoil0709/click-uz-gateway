6561f85f36324fa26225f1bf1cdf91ef3d3fd604
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-02-25 13:16:18 +0500

    click-uz gateway server created

---
+++ src/main/java/uz/pdp/clickuzgateway/ClickUzGatewayApplication.java
@@ -0,0 +1,12 @@
+package uz.pdp.clickuzgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickUzGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClickUzGatewayApplication.class, args);
+    }
+
+}
